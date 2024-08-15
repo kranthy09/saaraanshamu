0aadc763b0000b158d1ba059cd83a5cd2e7ae269
kranthu <g.kranthi2507@gmail.com>
2024-08-15 12:31:32 +0530

    models, aerich migrations

---
+++ project/app/db.py
@@ -0,0 +1,16 @@
+"""
+Aerich configuration variables
+"""
+
+import os
+
+
+TORTOISE_ORM = {
+    "connections": {"default": os.environ.get("DATABASE_URL")},
+    "apps": {
+        "models": {
+            "models": ["app.models.tortoise", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
